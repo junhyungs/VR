a20ca818cbd895ee71a58bdd52fc4eaf1545ae52
조현승 <danitech171791@gmail.com>
2024-06-11 19:32:11 +0900

    [feat] UI 상호작용 - WorldUI
    - 버튼 클릭, 슬라이더 변경 이벤트 연동

--- Assets/Scripts/WorldUI.cs
+++ Assets/Scripts/WorldUI.cs
@@ -8,14 +8,32 @@ public class WorldUI : MonoBehaviour
     [SerializeField] Button Btn_Temp;
     [SerializeField] Slider Slider_Temp;
 
+    private void OnEnable()
+    {
+        if(Btn_Temp != null)
+            Btn_Temp.onClick.AddListener(OnClick_TemporalBtn);
+
+        if (Slider_Temp != null)
+            Slider_Temp.onValueChanged.AddListener(OnValueChanged_Slider);
+    }
+
+    private void OnDisable()
+    {
+        if (Btn_Temp != null)
+            Btn_Temp.onClick.RemoveListener(OnClick_TemporalBtn);
+
+        if (Slider_Temp != null)
+            Slider_Temp.onValueChanged.RemoveListener(OnValueChanged_Slider);
+    }
+
     private void OnClick_TemporalBtn()
     {
         Debug.Log("버튼 눌려짐");
     }
 
-    private void OnValueChanged_Slider()
+    private void OnValueChanged_Slider(float value)
     {
-        Debug.Log($"슬라이더 변경 {Slider_Temp.value}");
+        Debug.Log($"슬라이더 변경 {value}");
     }
 
 }
