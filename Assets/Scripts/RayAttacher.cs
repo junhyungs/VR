036793d873d8c20cf47d2800283f54eac27fee14
조현승 <danitech171791@gmail.com>
2024-06-11 14:26:06 +0900

    [feat] 바나나건 - RayAttacher 기초구조

---
+++ Assets/Scripts/RayAttacher.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Unity.XR.CoreUtils;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RayAttacher : MonoBehaviour
+{
+    private IXRSelectInteractable _selectIntractable;
+
+}
